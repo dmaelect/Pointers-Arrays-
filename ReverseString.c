89d51912d7cea0ed4800cc1aac7c0030355e90dc
dmaelect <daryl@dmaelectronics.com>
2017-07-11 19:55:48 -0600

    Create ReverseString.c

---
+++ ReverseString.c
@@ -0,0 +1,21 @@
+void Reverse(char* array)
+{
+	char cTemp = 0;
+	int nCharLength = 0;
+	char *pBegin = array, *pEnd = array;
+
+	while(*pEnd++ != '\0')      //-- get the length of the incoming array
+	{
+		nCharLength++;
+	}
+
+  pEnd -= 2;                 //-- at this point pEnd is pointing at NULL
+						                 //-- I need to back the ponter up to the last char
+	while(pEnd >= pBegin)
+	{
+		cTemp = *pEnd;           //-- dereference pEnd and store it in cTemp
+		*pEnd-- = *pBegin;       //-- dereference pBegin and store it in dereferenced pEnd
+								             //-- decrement pEnd
+		*pBegin++ = cTemp;       //-- copy cTemp into dereferenced pBegin and increment pBegin
+	}
+}
