d77c75056149369b596e15cf0bfc8d35b9a3bce1
dmaelect <daryl@dmaelectronics.com>
2017-07-11 19:58:49 -0600

    Update and rename Re to ReverseString.c

--- Reverse String/ReverseString.c
+++ Reverse String/ReverseString.c
@@ -4,13 +4,13 @@ void Reverse(char* array)
 	int nCharLength = 0;
 	char *pBegin = array, *pEnd = array;
 
-	while(*pEnd++ != '\0')     //-- get the length of the incoming array
+	while(*pEnd++ != '\0')           //-- get the length of the incoming array
 	{
 		nCharLength++;
 	}
 
-	pEnd -= 2;                 //-- at this point pEnd is pointing at NULL
-						                 //-- I need to back the ponter up to the last char
+	pEnd -= 2;                       //-- at this point pEnd is pointing at NULL
+				         //-- I need to back the ponter up to the last char
 
 	while(pEnd >= pBegin)
 	{
